7b0b5c71687ee933098c07e75f29190529e58eee
zhaominn <150769172+zhaominn@users.noreply.github.com>
2025-09-12 12:37:45 +0900

    include bullet

---
+++ KarKartrider/KarKartrider/include/Bullet3Geometry/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Mac/Home/Documents/bullet3/src/Bullet3Geometry
+# Build directory: C:/Mac/Home/Documents/bullet3/build/src/Bullet3Geometry
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
