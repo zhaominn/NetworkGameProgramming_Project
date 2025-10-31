5bb242d70af0ceac15ebe08778c6e9391dce6beb
hyeondoong <sallybi@naver.com>
2025-10-31 10:56:21 +0900

    create : NetworkMgr에 필요한 함수 추가

--- KarKartrider/KarKartrider/NetworkMgr.cpp
+++ KarKartrider/KarKartrider/NetworkMgr.cpp
@@ -1,3 +1,20 @@
 #include "Pch.h"
 #include "NetworkMgr.h"
 
+NetworkMgr::NetworkMgr()
+{
+}
+
+NetworkMgr::~NetworkMgr()
+{
+	// 소켓 닫기
+	closesocket(sock);
+
+	// 윈속 종료
+	WSACleanup();
+}
+
+bool NetworkMgr::Initialize()
+{
+	return false;
+}
