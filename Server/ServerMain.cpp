e3516bb38bbf41ed570b09c860f9b2e918e3c846
zhaominn <150769172+zhaominn@users.noreply.github.com>
2025-10-31 10:54:19 +0900

    update : ServerMain 전역변수 정의

--- Server/ServerMain.cpp
+++ Server/ServerMain.cpp
@@ -3,4 +3,17 @@
 #pragma comment (lib,"WS2_32.LIB")
 #pragma comment (lib, "MSWSock.LIB")
 
+#include "protocol.h"
 #include "Player.h"
+#include <array>
+
+std::array<Player, MAX_USER> g_users;
+enum game_state{READY,INGAME,END};
+
+CRITICAL_SECTION g_CS;
+float g_ElapsedTime;
+
+int main()
+{
+	
+}
