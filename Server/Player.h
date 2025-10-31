3d2ff8246b943c318829eea140b496369df6abf9
zhaominn <150769172+zhaominn@users.noreply.github.com>
2025-10-31 10:48:48 +0900

    update : Player 멤버 변수 추가

--- Server/Player.h
+++ Server/Player.h
@@ -1,7 +1,14 @@
 #pragma once
+#include "protocol.h"
 
 class Player
 {
+public:
+	int id;
+	char name[NAME_SIZE];
+	short x, y;
+	SOCKET socket;
+
 public:
 	Player();
 	~Player();
---
+++ Server/ServerMain.cpp
@@ -0,0 +1,6 @@
+#include <WS2tcpip.h>
+#include <MSWSock.h>
+#pragma comment (lib,"WS2_32.LIB")
+#pragma comment (lib, "MSWSock.LIB")
+
+#include "Player.h"
