be302eb993a0b3dddf257392b10f266350bf36b8
neneee0181 <2022180024@tukorea.ac.kr>
2025-11-03 18:14:05 +0900

    update : login mode class 기본 틀 작성 # 11

--- KarKartrider/KarKartrider/LoginMode.cpp
+++ KarKartrider/KarKartrider/LoginMode.cpp
@@ -1,2 +1,34 @@
 #include "Pch.h"
 #include "LoginMode.h"
+
+void LoginMode::init()
+{
+}
+
+void LoginMode::mouseClick(int button, int state, int x, int y)
+{
+}
+
+void LoginMode::keyboard(unsigned char key, int x, int y)
+{
+}
+
+void LoginMode::specialKey(int key, int x, int y)
+{
+}
+
+void LoginMode::specialKeyUp(int key, int x, int y)
+{
+}
+
+void LoginMode::draw_model()
+{
+}
+
+void LoginMode::draw_bb()
+{
+}
+
+void LoginMode::finish()
+{
+}
--- KarKartrider/KarKartrider/LoginMode.h
+++ KarKartrider/KarKartrider/LoginMode.h
@@ -1,5 +1,25 @@
 #pragma once
-class LoginMode
+#include "Mode.h" 
+
+class LoginMode : public Mode
 {
+	LoginMode(){}
+	~LoginMode() { delete this; }
+
+    void init() override;
+
+    void mouseClick(int button, int state, int x, int y) override;
+
+    void keyboard(unsigned char key, int x, int y) override;
+
+    void specialKey(int key, int x, int y) override;
+
+    void specialKeyUp(int key, int x, int y) override;
+
+    void draw_model() override;
+
+    void draw_bb() override;
+
+    void finish() override;
 };
 
