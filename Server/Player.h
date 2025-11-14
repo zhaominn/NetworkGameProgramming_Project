3f1655d805fcafefe0c81562fe9519213181c0c6
zhaominn <150769172+zhaominn@users.noreply.github.com>
2025-11-14 10:45:34 +0900

    update: Room content #24

--- Server/Player.h
+++ Server/Player.h
@@ -1,12 +1,5 @@
 #pragma once
-#define _CRT_SECURE_NO_WARNINGS 
-
-#include <WS2tcpip.h>
-#include <MSWSock.h>
-#pragma comment (lib,"WS2_32.LIB")
-#pragma comment (lib, "MSWSock.LIB")
-#include "protocol.h"
-#include <iostream>
+#include "Room.h"
 
 class Player
 {
--- Server/Room.h
+++ Server/Room.h
@@ -1,5 +1,20 @@
 #pragma once
+#define _CRT_SECURE_NO_WARNINGS 
+
+#include <WS2tcpip.h>
+#include <MSWSock.h>
+#pragma comment (lib,"WS2_32.LIB")
+#pragma comment (lib, "MSWSock.LIB")
+#include "protocol.h"
+#include <iostream>
+
+class Player;
+
 class Room
 {
+public:
+	MAP_TYPE mapType;
+	Player* inRoomPlayers[MAX_USER];
 };
 
+Room g_room[MAX_USER];
