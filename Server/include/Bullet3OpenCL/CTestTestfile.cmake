b3b85c70b6c56829a08d104a68f6a57ddca17bd6
neneee0181 <2022180024@tukorea.ac.kr>
2025-11-18 16:16:22 +0900

    update : 서버 include, lib 폴더 내부 제거 #20
    서버쪽에서는 rigidbody 사용이 어려울듯?

--- Server/include/Bullet3OpenCL/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Mac/Home/Documents/bullet3/src/Bullet3OpenCL
-# Build directory: C:/Mac/Home/Documents/bullet3/build/src/Bullet3OpenCL
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- Server/include/LinearMath/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Mac/Home/Documents/bullet3/src/LinearMath
-# Build directory: C:/Mac/Home/Documents/bullet3/build/src/LinearMath
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
