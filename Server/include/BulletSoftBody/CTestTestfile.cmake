fc48e4e0addee9fbe6b94ed4119cd66210d02b79
neneee0181 <2022180024@tukorea.ac.kr>
2025-11-18 16:01:05 +0900

    create : bulletpyhsics사용을 위해 include 폴더랑 lib 폴더 추가 #20

---
+++ Server/include/BulletSoftBody/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Mac/Home/Documents/bullet3/src/BulletSoftBody
+# Build directory: C:/Mac/Home/Documents/bullet3/build/src/BulletSoftBody
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
