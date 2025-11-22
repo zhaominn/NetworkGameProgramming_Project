b4b4af30213d5d5b794f739f2e5dd62074ca8446
neneee0181 <2022180024@tukorea.ac.kr>
2025-11-23 00:57:33 +0900

    update : 부스터 패킷 제거 #26

--- KarKartrider/KarKartrider/protocol.h
+++ KarKartrider/KarKartrider/protocol.h
@@ -97,7 +97,6 @@ struct S2C_Move_All_Packet {
 	float yaw;
 	float face_rotation;
 	float body_rotation;
-	int boost_status;
 
 	float x;
 	float y;
@@ -114,7 +113,6 @@ struct S2C_Move_Packet
 	float yaw;
 	float face_rotation;
 	float body_rotation;
-	int boost_status;
 
 	float x;
 	float y;
