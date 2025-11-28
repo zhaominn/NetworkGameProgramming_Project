30aa2639089f2ec49c6a7e2c648c97cbebe2e5f5
neneee0181 <2022180024@tukorea.ac.kr>
2025-11-28 22:25:49 +0900

    update : ProcessPlayerCollision 방 기준 검사하도록 변경 #35

--- Server/collision.h
+++ Server/collision.h
@@ -5,4 +5,4 @@ void ApplyBounceReflection(Player& p, float pushX, float pushZ, float bounce);
 bool WallCollisionCheck(const AABB& box, int id, float& pushX, float& pushZ);
 bool PlayerCollisionCheck(int a, int b, float& pushX, float& pushZ);
 void ProcessWallCollision(Player& p, float& outPushX, float& outPushZ);
-void ProcessPlayerCollision(int myId, float& outPushX, float& outPushZ);
+void ProcessPlayerCollisionRoom(Room& room, int myId, float& outX, float& outZ);
