7b385194a12db65b53ea3c641ac95dbe4422a6b3
neneee0181 <2022180024@tukorea.ac.kr>
2025-11-30 02:47:19 +0900

    update : map2 벽 충돌 범위 수정 #35

--- KarKartrider/KarKartrider/LoginMode.cpp
+++ KarKartrider/KarKartrider/LoginMode.cpp
@@ -45,15 +45,15 @@ void LoginMode::init()
 	loadModelWithProgress<BarricateMap1Model>("baricate1.obj", "obj/road/", "finish", "box", glm::scale(glm::translate(glm::mat4(1.0f), glm::vec3(0.0, 0.0, -212.0)), glm::vec3(40.0, 10.0, 10.0)), road1_barricate, true, false);
 
 	loadModelWithProgress<RoadModel>("road_all_02.obj", "obj/road/", "road_all_2", "cube", glm::scale(glm::mat4(1.0f), glm::vec3(20.0, 20.0, 20.0)), road2, false, true);
-	loadModelWithProgress<BarricateMap1Model>("baricate1.obj", "obj/road/", "baricate", "box", glm::scale(glm::translate(glm::mat4(1.0f), glm::vec3(184.7, 0.0, 0.0)), glm::vec3(15.0, 15.0, 400.0)), road2_barricate, true, false);
-	loadModelWithProgress<BarricateMap1Model>("baricate1.obj", "obj/road/", "baricate", "box", glm::scale(glm::translate(glm::mat4(1.0f), glm::vec3(-184.7, 0.0, 0.0)), glm::vec3(15.0, 15.0, 400.0)), road2_barricate, true, false);
-	loadModelWithProgress<BarricateMap1Model>("baricate1.obj", "obj/road/", "baricate", "box", glm::scale(glm::translate(glm::mat4(1.0f), glm::vec3(0.0, 0.0, 184.7)), glm::vec3(400.0, 15.0, 15.0)), road2_barricate, true, false);
-	loadModelWithProgress<BarricateMap1Model>("baricate1.obj", "obj/road/", "baricate", "box", glm::scale(glm::translate(glm::mat4(1.0f), glm::vec3(0.0, 0.0, -184.7)), glm::vec3(400.0, 15.0, 15.0)), road2_barricate, true, false);
+	loadModelWithProgress<BarricateMap1Model>("baricate1.obj", "obj/road/", "baricate", "box", glm::scale(glm::translate(glm::mat4(1.0f), glm::vec3(184.7, 0.0, 0.0)), glm::vec3(15.0, 15.0, 380.0)), road2_barricate, true, false);
+	loadModelWithProgress<BarricateMap1Model>("baricate1.obj", "obj/road/", "baricate", "box", glm::scale(glm::translate(glm::mat4(1.0f), glm::vec3(-184.7, 0.0, 0.0)), glm::vec3(15.0, 15.0, 380.0)), road2_barricate, true, false);
+	loadModelWithProgress<BarricateMap1Model>("baricate1.obj", "obj/road/", "baricate", "box", glm::scale(glm::translate(glm::mat4(1.0f), glm::vec3(0.0, 0.0, 184.7)), glm::vec3(380.0, 15.0, 15.0)), road2_barricate, true, false);
+	loadModelWithProgress<BarricateMap1Model>("baricate1.obj", "obj/road/", "baricate", "box", glm::scale(glm::translate(glm::mat4(1.0f), glm::vec3(0.0, 0.0, -184.7)), glm::vec3(380.0, 15.0, 15.0)), road2_barricate, true, false);
 	//loadModelWithProgress<BarricateMap1Model>("baricate1.obj", "obj/road/", "baricate", "box", glm::scale(glm::translate(glm::mat4(1.0f), glm::vec3(0.0, 0.0, 0.0)), glm::vec3(331.0, 331.0, 331.0)), road2_barricate, true, false); //��� ��ֹ�
-	loadModelWithProgress<BarricateMap1Model>("baricate1.obj", "obj/road/", "baricate", "box", glm::scale(glm::translate(glm::mat4(1.0f), glm::vec3(145.3, 0.0, 0.0)), glm::vec3(15.0, 15.0, 315.0)), road2_barricate, true, false);
-	loadModelWithProgress<BarricateMap1Model>("baricate1.obj", "obj/road/", "baricate", "box", glm::scale(glm::translate(glm::mat4(1.0f), glm::vec3(-145.3, 0.0, 0.0)), glm::vec3(15.0, 15.0, 315.0)), road2_barricate, true, false);
-	loadModelWithProgress<BarricateMap1Model>("baricate1.obj", "obj/road/", "baricate", "box", glm::scale(glm::translate(glm::mat4(1.0f), glm::vec3(0.0, 0.0, 145.3)), glm::vec3(315.0, 15.0, 15.0)), road2_barricate, true, false);
-	loadModelWithProgress<BarricateMap1Model>("baricate1.obj", "obj/road/", "baricate", "box", glm::scale(glm::translate(glm::mat4(1.0f), glm::vec3(0.0, 0.0, -145.3)), glm::vec3(315.0, 15.0, 15.0)), road2_barricate, true, false);
+	loadModelWithProgress<BarricateMap1Model>("baricate1.obj", "obj/road/", "baricate", "box", glm::scale(glm::translate(glm::mat4(1.0f), glm::vec3(145.3, 0.0, 0.0)), glm::vec3(15.0, 15.0, 290.0)), road2_barricate, true, false);
+	loadModelWithProgress<BarricateMap1Model>("baricate1.obj", "obj/road/", "baricate", "box", glm::scale(glm::translate(glm::mat4(1.0f), glm::vec3(-145.3, 0.0, 0.0)), glm::vec3(15.0, 15.0, 290.0)), road2_barricate, true, false);
+	loadModelWithProgress<BarricateMap1Model>("baricate1.obj", "obj/road/", "baricate", "box", glm::scale(glm::translate(glm::mat4(1.0f), glm::vec3(0.0, 0.0, 145.3)), glm::vec3(290.0, 15.0, 15.0)), road2_barricate, true, false);
+	loadModelWithProgress<BarricateMap1Model>("baricate1.obj", "obj/road/", "baricate", "box", glm::scale(glm::translate(glm::mat4(1.0f), glm::vec3(0.0, 0.0, -145.3)), glm::vec3(290.0, 15.0, 15.0)), road2_barricate, true, false);
 
 	loadModelWithProgress<BarricateMap1Model>("baricate1.obj", "obj/road/", "finish", "box", glm::scale(glm::translate(glm::mat4(1.0f), glm::vec3(165.0, 0.0, 18.0)), glm::vec3(40.0, 1.0, 1.0)), road2_barricate, true, false);
 	loadModelWithProgress<BarricateMap1Model>("baricate1.obj", "obj/road/", "finish_ch", "box", glm::scale(glm::translate(glm::mat4(1.0f), glm::vec3(165.0, 0.0, 50.0)), glm::vec3(40.0, 1.0, 1.0)), road2_barricate, true, false);
@@ -61,49 +61,49 @@ void LoginMode::init()
 	glm::mat4 m1 = glm::mat4(1.0f);
 	m1 = glm::translate(m1, glm::vec3(174.7, 0.0, -175.3));
 	m1 = glm::rotate(m1, glm::radians(45.0f), glm::vec3(0.0, 1.0, 0.0));
-	m1 = glm::scale(m1, glm::vec3(5.0, 15.0, 15.0));
+	m1 = glm::scale(m1, glm::vec3(0.0, 0.0, 0.0));
 	loadModelWithProgress<BarricateMap1Model>("baricate1.obj", "obj/road/", "baricate", "box", m1, road2_barricate, true, false);
 
 	m1 = glm::mat4(1.0f);
 	m1 = glm::translate(m1, glm::vec3(174.7, 0.0, 175.3));
 	m1 = glm::rotate(m1, glm::radians(-45.0f), glm::vec3(0.0, 1.0, 0.0));
-	m1 = glm::scale(m1, glm::vec3(5.0, 15.0, 15.0));
+	m1 = glm::scale(m1, glm::vec3(0.0, 0.0, 0.0));
 	loadModelWithProgress<BarricateMap1Model>("baricate1.obj", "obj/road/", "baricate", "box", m1, road2_barricate, true, false);
 
 	m1 = glm::mat4(1.0f);
 	m1 = glm::translate(m1, glm::vec3(-174.7, 0.0, 175.3));
 	m1 = glm::rotate(m1, glm::radians(45.0f), glm::vec3(0.0, 1.0, 0.0));
-	m1 = glm::scale(m1, glm::vec3(5.0, 15.0, 15.0));
+	m1 = glm::scale(m1, glm::vec3(0.0, 0.0, 0.0));
 	loadModelWithProgress<BarricateMap1Model>("baricate1.obj", "obj/road/", "baricate", "box", m1, road2_barricate, true, false);
 
 	m1 = glm::mat4(1.0f);
 	m1 = glm::translate(m1, glm::vec3(-174.7, 0.0, -175.3));
 	m1 = glm::rotate(m1, glm::radians(-45.0f), glm::vec3(0.0, 1.0, 0.0));
-	m1 = glm::scale(m1, glm::vec3(5.0, 15.0, 15.0));
+	m1 = glm::scale(m1, glm::vec3(0.0, 0.0, 0.0));
 	loadModelWithProgress<BarricateMap1Model>("baricate1.obj", "obj/road/", "baricate", "box", m1, road2_barricate, true, false);
 
 	glm::mat4 m2 = glm::mat4(1.0f);
 	m2 = glm::translate(m2, glm::vec3(146.8, 0.0, -146.8));
 	m2 = glm::rotate(m2, glm::radians(45.0f), glm::vec3(0.0, 1.0, 0.0));
-	m2 = glm::scale(m2, glm::vec3(5.0, 15.0, 10.0));
+	m2 = glm::scale(m2, glm::vec3(0.0, 0.0, 0.0));
 	loadModelWithProgress<BarricateMap1Model>("baricate1.obj", "obj/road/", "baricate", "box", m2, road2_barricate, true, false);
 
 	m2 = glm::mat4(1.0f);
 	m2 = glm::translate(m2, glm::vec3(146.8, 0.0, 146.8));
 	m2 = glm::rotate(m2, glm::radians(-45.0f), glm::vec3(0.0, 1.0, 0.0));
-	m2 = glm::scale(m2, glm::vec3(5.0, 15.0, 10.0));
+	m2 = glm::scale(m2, glm::vec3(0.0, 0.0, 0.0));
 	loadModelWithProgress<BarricateMap1Model>("baricate1.obj", "obj/road/", "baricate", "box", m2, road2_barricate, true, false);
 
 	m2 = glm::mat4(1.0f);
 	m2 = glm::translate(m2, glm::vec3(-146.8, 0.0, 146.8));
 	m2 = glm::rotate(m2, glm::radians(45.0f), glm::vec3(0.0, 1.0, 0.0));
-	m2 = glm::scale(m2, glm::vec3(5.0, 15.0, 10.0));
+	m2 = glm::scale(m2, glm::vec3(0.0, 0.0, 0.0));
 	loadModelWithProgress<BarricateMap1Model>("baricate1.obj", "obj/road/", "baricate", "box", m2, road2_barricate, true, false);
 
 	m2 = glm::mat4(1.0f);
 	m2 = glm::translate(m2, glm::vec3(-146.8, 0.0, -146.8));
 	m2 = glm::rotate(m2, glm::radians(-45.0f), glm::vec3(0.0, 1.0, 0.0));
-	m2 = glm::scale(m2, glm::vec3(5.0, 15.0, 10.0));
+	m2 = glm::scale(m2, glm::vec3(0.0, 0.0, 0.0));
 	loadModelWithProgress<BarricateMap1Model>("baricate1.obj", "obj/road/", "baricate", "box", m2, road2_barricate, true, false);
 
 	loadModelWithProgress<KartModel>("3.obj", "obj/ui/", "countDown", "box", glm::scale(glm::mat4(1.0f), glm::vec3(1.0, 1.0, 1.0)), countDown, true, true);
